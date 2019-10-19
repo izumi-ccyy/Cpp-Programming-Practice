261ca9a82bc42ce127e3535e14cc0c012edc8e2d
Izumi <yfeng8102@gmail.com>
2019-10-19 21:52:20 +0800

    sayings

--- C++_Primer_Plus/Chap-12/12-2/string1.cpp
+++ C++_Primer_Plus/Chap-12/12-2/string1.cpp
@@ -1,41 +1,117 @@
-// stringbad.h -- Stringbad class methods
+// string1.h -- String class methods
 #include "string1.h"
 #include <cstring>
+#include <string.h>
+using std::cin;
 using std::cout;
 
 // initiallizing static class number
-int Stringbad::num_strings = 0;
+int String::num_strings = 0;
+
+// static method
+int String::HowMany()
+{
+    return num_strings;
+}
 
 // class methods
-// construct Stringbad from C string
-Stringbad::Stringbad(const char *s)
+// construct String from C string
+String::String(const char *s)
 {
     len = std::strlen(s);
     str = new char[len + 1];
     strcpy_s(str, len + 1, s);
     num_strings++;
-    cout << num_strings << ": \"" << str << "\" object created\n";
 }
 
-Stringbad::Stringbad()
+String::String()
 {
-    len = 4;
-    str = new char[4];
-    strcpy_s(str, 4, "C++");
+    len = 1;
+    str = new char[1];
+    str[0] = '\0';
     num_strings++;
-    cout << num_strings << ": \"" << str << "\" object created\n";
 }
 
-Stringbad::~Stringbad()
+String::String(const String &st)
 {
-    cout << "\"" << str << "\" object deleted, ";
+    num_strings++;
+    len = st.len;
+    str = new char[len + 1];
+    strcpy_s(str, len + 1, st.str);
+}
+
+String::~String()
+{
     --num_strings;
-    cout << num_strings << " left\n";
     delete[] str;
 }
 
-std::ostream &operator<<(std::ostream &os, const Stringbad &st)
+// overloaded operator methods
+//assign a String to a String
+String &String::operator=(const String &st)
+{
+    if (this == &st)
+        return *this;
+    delete[] str;
+    len = st.len;
+    str = new char[len + 1];
+    strcpy_s(str, len + 1, st.str);
+    return *this;
+}
+
+//assign a C string to a String
+String & String::operator=(const char *s)
+{
+    delete[] str;
+    len = std::strlen(s);
+    str = new char[len + 1];
+    strcpy_s(str, len + 1, s);
+    return *this;
+}
+
+// read-write char access for non-const String
+char & String::operator[](int i)
+{
+    return str[i];
+}
+
+// read-only char access for const String
+const char & String::operator[](int i) const
+{
+    return str[i];
+}
+
+// overload operator friends
+bool operator<(const String &st1, const String &st2)
+{
+    return (std::strcmp(st1.str, st2.str) < 0);
+}
+
+bool operator>(const String &st1, const String &st2)
+{
+    return st2 < st1;
+}
+
+bool operator==(const String &st1, const String &st2)
+{
+    return (std::strcmp(st1.str, st2.str) == 0);
+}
+
+// simple String output
+ostream &operator<<(ostream &os, const String &st)
 {
     os << st.str;
     return os;
+}
+
+// quick and dirty String input
+istream &operator>>(istream &is, String &st)
+{
+    char temp[String::CINLIM];
+    is.get(temp, String::CINLIM);
+    if (is)
+        st = temp;
+    while (is && is.get() != '\n')
+        continue;
+    return is;
 }
--- C++_Primer_Plus/Chap-12/12-2/string1.h
+++ C++_Primer_Plus/Chap-12/12-2/string1.h
@@ -4,19 +4,34 @@ using std::istream;
 using std::ostream;
 #ifndef STRING1_H_
 #define STRING1_H_
-class Stringbad
+class String
 {
 private:
     char *str;
     int len;
     static int num_strings;
+    static const int CINLIM = 80;
 
 public:
-    Stringbad(const char *s);
-    Stringbad();
-    ~Stringbad();
-    // friend function
-    friend std::ostream &operator<<(std::ostream &os, const Stringbad &st);
+    // constructors and other methods
+    String(const char *s);
+    String();
+    String(const String &);
+    ~String();
+    int length() const { return len; }
+    // overloading operator methods
+    String &operator=(const String &);
+    String &operator=(const char *);
+    char &operator[](int i);
+    const char &operator[](int i) const;
+    // overloaded operator friends
+    friend bool operator<(const String &st1, const String &st2);
+    friend bool operator>(const String &st1, const String &st2);
+    friend bool operator==(const String &st1, const String &st2);
+    friend ostream &operator<<(ostream &os, const String &st);
+    friend istream &operator>>(istream &is, String &st);
+    // static function
+    static int HowMany();
 };
 
 #endif
